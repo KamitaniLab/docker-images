c0a9cd3d4dcb9778b70325cd428d722cd6b20a37
mitsuaki <mitsuaki@atr.jp>
2021-03-02 18:52:59 +0900

    modify image name for caffe

--- caffe/build.sh
+++ caffe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="kamitanilab/cuda11.0_caffe"
+IMAGE_NAME="kamitanilab/caffe-cuda11"
 
 docker build -t ${IMAGE_NAME}:1.0-$(date +%Y%m%d) .
 docker tag ${IMAGE_NAME}:1.0-$(date +%Y%m%d) ${IMAGE_NAME}:latest
