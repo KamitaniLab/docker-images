09196ce106ed0a64f9fc2d726ee80b9517c2b6ba
mitsuaki <mitsuaki@atr.jp>
2021-03-02 19:39:31 +0900

    Separate docker images for caffe and caffe-deepsim

--- caffe/build_caffe-1.0.sh
+++ caffe/build_caffe-1.0.sh
@@ -2,6 +2,7 @@
 
 IMAGE_NAME="kamitanilab/caffe-cuda11"
 
+cp Dockerfile_caffe-1.0  Dockerfile
 docker build -t ${IMAGE_NAME}:1.0-$(date +%Y%m%d) .
 docker tag ${IMAGE_NAME}:1.0-$(date +%Y%m%d) ${IMAGE_NAME}:latest
 
